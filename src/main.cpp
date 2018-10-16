9f0b91260c369a4d3fdbcef2a666c95ce5779e87
Francis Irizarry <tuxwonder7@gmail.com>
2018-10-16 02:31:59 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,5 @@
-/******************************************************************************
-  Title          : main.cpp
-  Author         : 
-  Created on     : October  7, 2018
-  Description    : Displays random peculiar facts
-  Purpose        : To exercise collaborative source code development
-  Usage          : nuttyfact
-  Build with     : g++ -Wall -g -o oddities  main.cpp
-  Modifications  :
-  Notes:
-  The number N below is the number of people on each team.
-  This program prints N peculiar facts, called "oddities" here.
-  The main program calls N functions, each with the syntax defined
-  by the output_function typedef defined below. Each function must be 
-  named 
-       username_oddity
 
-  where username is the GitHub username of its author.
-  For example, 
-      output_function  stewartweiss_oddity;
-
-  Oddities are found in the file "oddities.txt" in the project source directory
- 
-******************************************************************************/
-
-#include <iostream>
-#include <fstream>
-#include <cstdlib>
-#include <cstdio>
-#include <cstring>
-#include <limits>
-#include <sstream>
-#include <stdlib.h>
-//#include <random>
-
-using namespace std;
-
-/******************************************************************************/
-
-// The syntax of the output function that must be used to print an oddity.
-typedef ostream& (*output_function)  (const ostream & out);
-
-
-/******************************************************************************
-        
-                        All Function Definitions
-
-
-******************************************************************************/
+//typedef ostream& (*output_function) (const ostream & out);
 
 
 
@@ -58,7 +11,9 @@ fstream& StrFromLine(fstream& file, unsigned int Exactlocation){
 	return file;
 }
 
-ostream& anupamdas104_oddity(const ostream & out){}
+ostream& anupamdas104_oddity(ostream & out){
+    return out;
+}
 
 
 int main(int argc, char* argv[] )
@@ -76,13 +31,8 @@ int main(int argc, char* argv[] )
 	StrFromLine(odditiesFile, (rand() % 15 + 1));
 	string oddityStr;
 	getline(odditiesFile, oddityStr);
-	output_function testFunc = anupamdas104_oddity;
-	testFunc(cout << oddityStr << endl);
-
-	
-	
-
+	output_function typeFunc;
+	typeFunc = anupamdas104_oddity;
+	typeFunc(cout << oddityStr << endl);
     return 0;
 }
-
-
